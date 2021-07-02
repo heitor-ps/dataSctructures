231977fff3913d7e55871c7b40d066f273805c6f
Heitor Pereira <heytorpg7@gmail.com>
2021-07-02 01:42:31 -0300

    lógica dos métodos modificada

--- fila/src/Fila.java
+++ fila/src/Fila.java
@@ -1,31 +1,33 @@
 public class Fila {
     public Object[] fila;
     public int posicaoFila;
+    public int tamanhoFila;
 
 
     public Fila(){
-        this.posicaoFila = 0;
         this.fila = new Object[10];
+        this.posicaoFila = fila.length-1;
+        this.tamanhoFila = fila.length-1;
+
     }
 
     public boolean filaVazia(){
-        if (this.posicaoFila == -1){
+        if (this.tamanhoFila){
             return true;
         }else return false;
     }
 
-    public void enfileirar(Object valor){
-        if (this.posicaoFila < this.fila.length){
-            this.fila[++posicaoFila] = valor;
-        }
+    public void enqueue(Object valor){
+        if (this.tamanhoFila <= fila.length-1 && this.tamanhoFila >= 0){
+            this.fila[this.tamanhoFila--] = valor;
+        }else System.out.println("Impossible insertion (overflow)");
     }
 
-
-    public Object desenfileirar(){
+    public Object dequeue(){
         if (filaVazia()){
             return null;
         }
-        return this.fila[this.posicaoFila--];
+        else return this.fila[this.posicaoFila--];
     }
 
 }
