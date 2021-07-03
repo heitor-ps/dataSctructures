65dd6cae3d152ed099b1d5c1b3fa2c958711ba55
Heitor Pereira <heytorpg7@gmail.com>
2021-07-03 00:49:42 -0300

    fizzBuzz - tamanho de array personalizado, lógica modificada

--- fizzBuzz/src/fizzBuzz.java
+++ fizzBuzz/src/fizzBuzz.java
@@ -1,16 +1,25 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
+
 public class fizzBuzz {
     public static void main(String[] args) {
-        int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert an integer");
+        int[] number = new int[sc.nextInt()];
 
-        for (int i = 0; i < number.length; i++) {
-            if (number[i] % 3 == 0) {
-                System.out.println("fizz at index " + number[i]);
-            }
-            if (number[i] % 5 == 0) {
-                System.out.println("buzz at index " + number[i]);
-            }
-            if (number[i] % 3 == 0 && number[i] % 5 == 0) {
-                System.out.println("fizz buzz at index " + number[i]);
+        for (int j = 1; j <= number.length;j++){
+            number[j-1]=j;
+        }
+
+        for (int i = 0; i < number.length; i++){
+            if (number[i]%15==0){
+                System.out.println("fizz buzz at index "+number[i]);
+            } else if (number[i]%3==0){
+                System.out.println("fizz at index "+number[i]);
+            } else if (number[i]%5==0){
+                System.out.println("buzz at index "+number[i]);
             }
         }
     }
