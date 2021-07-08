c6d36e8f5a5c23a8dd6f543f3528a0a247f416a6
Heitor Pereira <heytorpg7@gmail.com>
2021-07-08 05:25:35 -0300

    Método driver adicionado a classe

--- pilha/src/Pilha.java
+++ pilha/src/Pilha.java
@@ -32,13 +32,16 @@ public class Pilha {
         }
         return this.pilha[this.posicaoPilha--];
     }
+    public static void main(String[] args) {
+        Pilha p = new Pilha();
 
-
-
-
-
-
-
-
-
-}
+        p.empilhar("Portuguesa");
+        p.empilhar("Frango catupiry");
+        p.empilhar("Calabresa");
+        p.empilhar("Quatro Queijos");
+        p.empilhar(10);
+        while (p.pilhaVazia() == false){
+            System.out.println(p.desempilhar());
+        }
+    }
+   }
