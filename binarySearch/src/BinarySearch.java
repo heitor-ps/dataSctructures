8665416371c3a8404668ee4e10f43a9bacb8f4fe
Heitor Pereira <heytorpg7@gmail.com>
2021-07-16 09:47:46 -0300

    binarySearch - método principal implementado

--- binarySearch/src/BinarySearch.java
+++ binarySearch/src/BinarySearch.java
@@ -1,6 +1,7 @@
 import java.util.Random;
 public class BinarySearch {
     int[] rndArray;
+
     public int randomizer(){
         Random rd = new Random();
         int upper = 250;
@@ -14,7 +15,44 @@ public class BinarySearch {
             rndArray[i] = randomizer();
         }
     }
+
+    // Função de sorting (insertionSort)
+    void sorter(int[] vector){
+        int size = vector.length;
+
+        for (int i=0; i<size;i++){
+            int target = vector[i];
+            int j = i - 1;
+
+            while (j >= 0 && vector[j] > target){
+                vector[j+1] = vector[j];
+                j-=1;
+            }
+            vector[j+1] = target;
+        }
+    }
+
+
     // Fazer a partir daqui
+    public int binarySearch(int[] vector, int left, int right, int target){
+
+        if (right >= left){
+            int mid = left + (right-left)/2;
+
+            if (vector[mid] == target){
+                return mid;
+            }
+
+            if (vector[mid] > target){
+                return binarySearch(vector, left-0, mid - 1,target-0);
+            }
+
+            if (vector[mid] < target){
+                return binarySearch(vector,mid+1,right-0,target-0);
+            }
+        }
+        return -1;
+    }
 
     void printer(int[] vector){
         int size = vector.length;
--- binarySearch/src/Main.java
+++ binarySearch/src/Main.java
@@ -3,14 +3,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         BinarySearch bs = new BinarySearch();
+        int result, target;
 
         System.out.println("Insert the number os elements for the array");
         bs.arrayGenerator(sc.nextInt());
         System.out.print("Generated array: ");
         bs.printer(bs.rndArray);
         System.out.print("Now sorting...\n");
-
+        bs.sorter(bs.rndArray);
         System.out.print("Sorted array: ");
         bs.printer(bs.rndArray);
+        System.out.println("Enter the value for the search:");
+        target = sc.nextInt();
+        result = bs.binarySearch(bs.rndArray, 0, bs.rndArray.length-1,target);
+        if (result == -1){
+            System.out.println("Cant find the element");
+        } else {
+            System.out.println("Elemente found at index " + (result+1));
+        }
     }
 }
